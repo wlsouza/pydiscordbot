8da2f790bbb583c7093a6fe08532f9f2aa923043
Wellington Lorenço de Souza <wlsouza@protonmail.com>
2021-07-15 18:57:11 -0300

    feat: module self update in db if it's already there

--- pydiscordbot/ext/modules/countdown/countdown.py
+++ pydiscordbot/ext/modules/countdown/countdown.py
@@ -1,29 +1,41 @@
 from discord.ext import commands
 from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy import or_ 
 
 from ext import db
 from ext.db.models import Module
 
 class Countdown(commands.Cog):
 
+    name = "Countdown"
+    path = "ext.modules.countdown"
+    disableable = True
+    emoji = "⏰" 
+
     def __init__(self, app):
         self.app = app
         self.session = db.Session()
-        if self._module_in_db():
-            self._update_module_in_db()
-        else:
-            self._insert_module_in_db()
+        self._init_module_in_db()
 
     # Auxiliary methods
-    def _insert_module_in_db(self):
-        module = Module(
-            name="Countdown",
-            path="ext.modules.countdown",
-            disableable=True,
-            emoji="⏰"
-        )
-        self.session.add(module)
-        self.session.commit()
+
+    def _init_module_in_db(self):
+        try:
+            module = self.session.query(Module).filter(or_(Module.name == self.name, Module.path == self.path)).one()
+            module.name = self.name
+            module.path = self.path
+            module.disableable = self.disableable
+            module.emoji = self.emoji
+            self.session.commit()
+        except NoResultFound:
+            module = Module(
+                name=self.name,
+                path=self.path,
+                disableable=self.disableable,
+                emoji=self.emoji
+            )
+            self.session.add(module)
+            self.session.commit()
 
     def _module_in_db(self):
         try:
