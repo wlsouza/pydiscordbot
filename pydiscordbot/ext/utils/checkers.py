ab6586f538e92cb1cd02d16953eb41dd7ef0d04b
Wellington Lorenço de Souza <wlsouza@protonmail.com>
2021-07-18 15:25:39 -0300

    feat: add is_guild_owner check to use in guildmodulemanager

--- pydiscordbot/ext/utils/checkers.py
+++ pydiscordbot/ext/utils/checkers.py
@@ -1,4 +1,6 @@
+from discord.ext import commands
 from sqlalchemy.orm.exc import NoResultFound
+
 from ext.db import session
 from ext.db.models import Guild
 from ext.config import settings
@@ -10,3 +12,9 @@ def get_prefix(client, message):
         return guild.prefix
     except NoResultFound:
         return settings.default_command_prefix
+
+def is_guild_owner():
+    def predicate(ctx):
+        return ctx.guild is not None and ctx.guild.owner_id == ctx.author.id
+    return commands.check(predicate)
+
