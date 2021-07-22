c691a7ad1b7da4ba940b26f75786b40bae4d976a
Wellington Lorenço de Souza <wlsouza@protonmail.com>
2021-07-22 12:09:26 -0300

    feat[utils/checkers]: add a checker to check if module is enabled in guild

--- pydiscordbot/ext/utils/checkers.py
+++ pydiscordbot/ext/utils/checkers.py
@@ -1,14 +1,14 @@
 from discord.ext import commands
 from sqlalchemy.orm.exc import NoResultFound
 
-from ext.db import session
-from ext.db.models import Guild
 from ext.config import settings
+from ext.db import models
+from ext.db import session
 
 
 def get_prefix(client, message): 
     try:
-        guild = session.query(Guild).filter(Guild.id==message.guild.id).one()
+        guild = session.query(models.Guild).filter(models.Guild.id==message.guild.id).one()
         return guild.prefix
     except NoResultFound:
         return settings.default_command_prefix
@@ -18,3 +18,19 @@ def is_guild_owner():
         return ctx.guild is not None and ctx.guild.owner_id == ctx.author.id
     return commands.check(predicate)
 
+def module_is_enabled_in_guild():
+    def predicate(ctx):
+        try:
+            enabled = (
+                session.query(models.GuildModule)
+                .join(models.Module)
+                .filter(
+                    models.GuildModule.guild_id == ctx.guild.id,
+                    models.Module.name == ctx.cog.name,
+                    models.GuildModule.enabled == True
+                ).one()
+            )
+            return True
+        except NoResultFound:
+            return False
+    return commands.check(predicate)
