8a7e7c38e285aa773092f1cc661490b2adde1bfb
Wellington Lorenço de Souza <wlsouza@protonmail.com>
2021-07-26 17:59:00 -0300

    feat[app.py]: initiate guildconfig module as a required module

---
+++ pydiscordbot/app.py
@@ -0,0 +1,34 @@
+from discord.ext import commands
+from dislash import SlashClient
+
+from ext.config import settings
+from ext.modules import  botmodulemanager, guildmodulemanager, guildconfig, countdown
+from ext.utils import checkers
+from ext import db
+
+
+def create_app():
+    
+    # creating bot
+    app = commands.Bot(command_prefix=checkers.get_prefix)
+    slash = SlashClient(app)
+
+    # create database
+    db.init_app()
+
+    # Initiating required modules
+    botmodulemanager.init_app(app) # Required module
+    guildmodulemanager.init_app(app) # Required module
+    guildconfig.init_app(app) # Required module
+
+    # Initiating secondary modules
+    countdown.init_app(app)
+
+    return app.run(settings.TOKEN_BOT)
+
+if __name__ == "__main__":
+    create_app()
+
+
+
+
