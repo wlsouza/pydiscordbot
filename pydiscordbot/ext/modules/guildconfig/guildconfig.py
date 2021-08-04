4294ad7c6b3b44cde1e0e684c94cab8e74941b14
Wellington Lorenço de Souza <wlsouza@protonmail.com>
2021-08-04 17:03:30 -0300

    feat: add init_guild_in_db method

--- pydiscordbot/ext/modules/guildconfig/guildconfig.py
+++ pydiscordbot/ext/modules/guildconfig/guildconfig.py
@@ -1,7 +1,7 @@
 from discord.ext import commands
 # from dislash import SelectMenu, SelectOption
 # from asyncio.exceptions import CancelledError, TimeoutError
-from sqlalchemy.orm.exc import NoResultFound, 
+from sqlalchemy.orm.exc import NoResultFound
 
 from ext.modules import Module
 from ext.db import models
@@ -12,21 +12,29 @@ class GuildConfig(Module):
 
     # Auxiliary methods
 
-    def _init_guild_in_db(self, ctx):
-        guild = models.Guild(
-            id = ctx.guild.id 
-        )
-        self.session.add(guild)
-        self.session.commit()
+    def _init_guild_in_db(self, guild):
+        #I used this method to find out if the guild already exists because
+        #I didn't want to use a sqlite's specific exception. 
+        # (since I intend to migrate to another db in the future)
+        try:
+            self.session.query(models.Guild).filter(
+                models.Guild.id == guild.id
+            ).one()
+        except NoResultFound:
+            guild = models.Guild(
+                id = guild.id 
+            )
+            self.session.add(guild)
+            self.session.commit()
 
             
     # Command methods        
-    @commands.Cog.command()
+    @commands.command()
     async def change_prefix(self, ctx):
         raise NotImplementedError("To be implemented")
 
     # Listeners methods
 
     @commands.Cog.listener()
-    async def on_guild_join(self, ctx):
-        raise NotImplementedError("To be implemented")
+    async def on_guild_join(self, guild):
+        self._init_guild_in_db(guild)
